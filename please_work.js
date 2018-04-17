ca19b85fc652ddfbde86b597e1ebe8f61c4a8db3
Danny  Lam <dl01258@srcs.loc>
2018-04-17 12:17:23 -0700

    this is progress

---
+++ please_work.js
@@ -0,0 +1,8 @@
+function createBlocks() {
+    var numblocks = 20;
+    for (i = 0; i < numblocks; i++) {
+        var elem = document.getElementById('row1');
+        elem.className = "blocks";
+        elem.id = "blocks" + 1;
+    }
+}
