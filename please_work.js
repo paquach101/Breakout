b47b10b237bc5e02b8f7ecdd96b2063da6d2394a
Pat the Destroyer213 <phatquach@Pats-iMac.local>
2018-04-18 23:09:26 -0700

    k

--- please_work.js
+++
@@ -1,8 +0,0 @@
-function createBlocks() {
-    var numblocks = 20;
-    for (i = 0; i < numblocks; i++) {
-        var elem = document.getElementById('row1');
-        elem.className = "blocks";
-        elem.id = "blocks" + 1;
-    }
-}
